b06076aadd876253e6087a50a3d0c74e1f1ef98c
flowourdaddy <infernoprodukcija@gmail.com>
2026-04-16 17:38:34 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return "You have been fucked by Flowo & OPSanGZ (Thanks for IP):)"
-
+    ip = request.headers.get('X-Forwarded-For', request.remote_addr)
+    print(f"User IP: {ip}")
+    return f"Your IP is {ip}"
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
